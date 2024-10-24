7fed52a07856796c54bf59d1742012c97d956da4
YeChan Kim <az20058@naver.com>
2024-10-24 22:11:52 +0900

    🎉Start: 프로젝트 시작

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@api', replacement: '/src/api' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@commons', replacement: '/src/components/commons' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@layouts', replacement: '/src/layouts' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@utils', replacement: '/src/utils' },
+      { find: '@svgs', replacement: '/src/assets/svgs' },
+      { find: '@images', replacement: '/src/assets/images' },
+      { find: '@store', replacement: '/src/store' },
+    ],
+  },
 });
