08950cd2ddf0cb888d18cf27e9d56fd53b89a185
YeChan Kim <az20058@naver.com>
2024-10-25 00:03:44 +0900

    🎉Start: 프로젝트 시작

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@api', replacement: '/src/api' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@common', replacement: '/src/components/common' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@layouts', replacement: '/src/layouts' },
-      { find: '@hooks', replacement: '/src/hooks' },
-      { find: '@images', replacement: '/src/assets/images' },
-      { find: '@store', replacement: '/src/store' },
-      { find: '@styles', replacement: '/src/styles' },
-      { find: '@routes', replacement: '/src/routes' },
-    ],
+    alias: [{ find: '@/', replacement: path.resolve(__dirname, './src/') }],
   },
 });
