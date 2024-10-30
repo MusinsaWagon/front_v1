7015127bd0c2727d09d4e35129016d4a566b3a63
YeChan Kim <az20058@naver.com>
2024-10-31 02:55:37 +0900

    🎨 Design: 상품 등록 페이지 컴포넌트 및 페이지 UI 완성

--- src/pages/enrollProd/EnrollProd.tsx
+++ src/pages/enrollProd/EnrollProd.tsx
@@ -1,48 +1,23 @@
+import styled from 'styled-components';
 import HeaderSection from '../../components/common/HeaderSection';
-import Image from '../../components/common/Image';
-import UserInput from '../../components/common/UserInput';
-import { getInputInfos } from '../../hooks/modal/getModalInfo';
-import * as E from '../../styles/enrollProd/EnrollProd.styled';
+import BodySection from '../../components/enrollProd/BodySection';
 
 interface EnrollProdProps {
   type: string;
 }
 
 export default function EnrollProd({ type }: EnrollProdProps) {
-  const inputInfos = getInputInfos(type);
-  const list = ['MUSINSA', 'ZIGZAG', 'ABLY'];
   return (
-    <E.Wrapper>
+    <Wrapper>
       <HeaderSection type="product" />
-      <E.InputWrapper>
-        <UserInput
-          type={inputInfos.type}
-          placeholder={inputInfos.placeholder}
-          label={inputInfos.label}
-          list={list}
-        />
-        <UserInput
-          type={inputInfos.type2}
-          placeholder={inputInfos.placeholder2}
-          label={inputInfos.label2}
-        />
-      </E.InputWrapper>
-      <E.ProdWrapper>
-        <label>등록하려는 상품 정보</label>
-        <E.ProdInfo>
-          <E.ProdHeader>
-            <span>[{'PRENDA'}]</span>
-            <E.Line width="100%" />
-            <span>PRDA DENIM PATCH CREWNECK SHIRT</span>
-            <label>￦ 39,900</label>
-          </E.ProdHeader>
-          <E.ImageWrapper>
-            {Array.from({ length: 3 }).map((_, index) => (
-              <Image src="" key={index} width="31.61%" aspectRatio="1/1" />
-            ))}
-          </E.ImageWrapper>
-        </E.ProdInfo>
-      </E.ProdWrapper>
-    </E.Wrapper>
+      <BodySection type={type} />
+    </Wrapper>
   );
 }
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 4.15% 5.47%;
+  box-sizing: border-box;
+  margin-top: 8.63%;
+`;
