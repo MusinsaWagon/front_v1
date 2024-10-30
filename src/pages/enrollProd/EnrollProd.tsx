e2f9cb87d0159344cd6e63fa7ff915ad87c8c96e
YeChan Kim <az20058@naver.com>
2024-10-31 01:35:49 +0900

    🎨 Design: 상품 등록 페이지 디자인 추가

--- src/pages/enrollProd/EnrollProd.tsx
+++ src/pages/enrollProd/EnrollProd.tsx
@@ -1,10 +1,48 @@
 import HeaderSection from '../../components/common/HeaderSection';
+import Image from '../../components/common/Image';
+import UserInput from '../../components/common/UserInput';
+import { getInputInfos } from '../../hooks/modal/getModalInfo';
 import * as E from '../../styles/enrollProd/EnrollProd.styled';
 
-export default function EnrollProd() {
+interface EnrollProdProps {
+  type: string;
+}
+
+export default function EnrollProd({ type }: EnrollProdProps) {
+  const inputInfos = getInputInfos(type);
+  const list = ['MUSINSA', 'ZIGZAG', 'ABLY'];
   return (
     <E.Wrapper>
       <HeaderSection type="product" />
+      <E.InputWrapper>
+        <UserInput
+          type={inputInfos.type}
+          placeholder={inputInfos.placeholder}
+          label={inputInfos.label}
+          list={list}
+        />
+        <UserInput
+          type={inputInfos.type2}
+          placeholder={inputInfos.placeholder2}
+          label={inputInfos.label2}
+        />
+      </E.InputWrapper>
+      <E.ProdWrapper>
+        <label>등록하려는 상품 정보</label>
+        <E.ProdInfo>
+          <E.ProdHeader>
+            <span>[{'PRENDA'}]</span>
+            <E.Line width="100%" />
+            <span>PRDA DENIM PATCH CREWNECK SHIRT</span>
+            <label>￦ 39,900</label>
+          </E.ProdHeader>
+          <E.ImageWrapper>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <Image src="" key={index} width="31.61%" aspectRatio="1/1" />
+            ))}
+          </E.ImageWrapper>
+        </E.ProdInfo>
+      </E.ProdWrapper>
     </E.Wrapper>
   );
 }
--- src/styles/enrollProd/EnrollProd.styled.ts
+++ src/styles/enrollProd/EnrollProd.styled.ts
@@ -4,4 +4,79 @@ export const Wrapper = styled.div`
   width: 100%;
   padding: 4.15% 5.47%;
   box-sizing: border-box;
+  margin-top: 8.63%;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 19px 0;
+`;
+
+export const ProdWrapper = styled.div`
+  margin-top: 19px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 7px 0;
+  label {
+    color: rgba(12, 12, 12, 0.5);
+    font-size: 7px;
+    letter-spacing: 0.24px;
+    font-weight: bold;
+    text-align: left;
+    width: 100%;
+  }
+`;
+
+export const ProdInfo = styled.div`
+  box-shadow: inset 0 0 0 1.07px #cfcfcf;
+  padding: 18px 3.35% 11.07px 3.35%;
+`;
+
+export const ProdHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    color: #1d1d1d;
+    width: 100%;
+    text-align: left;
+    font-size: 10px;
+    font-weight: bold;
+  }
+
+  span:nth-last-child(2) {
+    color: #0c0c0c;
+    width: 100%;
+    text-align: left;
+    font-size: 10.81px;
+    font-weight: 800;
+    line-height: 130%;
+  }
+
+  label {
+    margin-top: 9.43%;
+    text-align: right;
+    width: 100%;
+    font-weight: 800;
+    font-size: 14px;
+    color: #0c0c0c;
+  }
+`;
+
+export const Line = styled.div<{ width: string }>`
+  height: 0.57px;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: ${({ width }) => width};
+  margin: 6px 0;
+`;
+
+export const ImageWrapper = styled.div`
+  margin-top: 4.25%;
+  display: flex;
+  justify-content: center;
+  gap: 2.7%;
 `;
