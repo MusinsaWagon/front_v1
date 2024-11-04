f2ba5ac754fe2a3e1f00bc16f47a72f0828f015f
YeChan Kim <az20058@naver.com>
2024-11-04 19:00:27 +0900

    🎨 Design : LineHeader 컴포넌트 디자인 수정 및 완성

--- src/components/common/LineHeader.tsx
+++ src/components/common/LineHeader.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { COLORS } from '../../constant/theme';
 
 interface LineHeaderProps {
   width: string;
@@ -19,17 +20,30 @@ export default function LineHeader({
   msg2,
   price,
 }: LineHeaderProps) {
+  const isFullWidth = width === '100%';
+
   return (
     <ProdHeader width={width} $padding={padding}>
       <span>[{msg}]</span>
       <Line $lineWidth={lineWidth} />
       <span>{msg2}</span>
-      <label>￦ {price}</label>
+      <PriceWrapper $isFullWidth={isFullWidth} saleRate={saleRate}>
+        {saleRate && (
+          <SaleInfo>
+            <div />
+            <span>{saleRate} %</span>
+          </SaleInfo>
+        )}
+        <Price $isFullWidth={isFullWidth} saleRate={saleRate}>
+          <label>현재가</label>
+          <label>￦ {price}</label>
+        </Price>
+      </PriceWrapper>
     </ProdHeader>
   );
 }
 
-export const ProdHeader = styled.div<{ width: string; $padding?: string }>`
+const ProdHeader = styled.div<{ width: string; $padding?: string }>`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -54,20 +68,74 @@ export const ProdHeader = styled.div<{ width: string; $padding?: string }>`
     font-weight: 800;
     line-height: 130%;
   }
-
-  label {
-    margin-top: 10px;
-    text-align: right;
-    width: 100%;
-    font-weight: 800;
-    font-size: 14px;
-    color: #0c0c0c;
-  }
 `;
 
-export const Line = styled.div<{ $lineWidth: string }>`
+const Line = styled.div<{ $lineWidth: string }>`
   height: 0.57px;
   background-color: rgba(0, 0, 0, 0.5);
   width: ${({ $lineWidth }) => $lineWidth};
   margin: 6px 0;
+`;
+
+const PriceWrapper = styled.div<{
+  $isFullWidth: boolean;
+  saleRate?: number | null;
+}>`
+  height: 25px;
+  display: flex;
+  width: 100%;
+  align-items: end;
+  justify-content: ${({ $isFullWidth, saleRate }) =>
+    $isFullWidth ? 'flex-end' : saleRate ? 'space-between' : 'flex-start'};
+`;
+
+const SaleInfo = styled.div`
+  width: 62px;
+  height: 16px;
+  background-color: ${COLORS.red};
+  border-radius: 9px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2.94px;
+
+  & > div {
+    width: 0;
+    height: 0;
+    border-left: 3.32px solid transparent;
+    border-right: 3.32px solid transparent;
+    border-bottom: 5.75px solid white;
+    transform: rotate(180deg);
+  }
+
+  span {
+    font-size: 11px;
+    font-weight: 800;
+    color: white;
+  }
+`;
+
+const Price = styled.div<{ $isFullWidth: boolean; saleRate?: number | null }>`
+  display: flex;
+  align-items: end;
+  flex-direction: ${({ $isFullWidth, saleRate }) =>
+    $isFullWidth || saleRate ? 'column' : 'row'};
+  gap: 6px;
+
+  & > label {
+    color: #0c0c0c;
+    text-align: right;
+  }
+
+  & > label:first-child {
+    font-size: 6px;
+    line-height: 8px;
+    font-weight: bold;
+    color: rgba(12, 12, 12, 0.5);
+  }
+
+  & > label:last-child {
+    font-weight: 800;
+    font-size: 14px;
+  }
 `;
