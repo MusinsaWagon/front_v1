ce7fddeb8c1be8f71a92543b166c45f287600d15
YeChan Kim <az20058@naver.com>
2024-11-12 09:21:18 +0900

    🎨 Design : 프롭스/디자인 구조 수정 및 조건부로 msg, priceWrapper 렌더링

--- src/components/common/LineHeader.tsx
+++ src/components/common/LineHeader.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { COLORS } from '../../constant/theme';
+import Review from '../productDetail/Review';
 
 interface LineHeaderProps {
   width: string;
@@ -7,8 +8,9 @@ interface LineHeaderProps {
   padding?: string;
   msg: string;
   msg2: string;
-  price: number;
+  price?: number;
   saleRate?: number | null;
+  review?: number[];
 }
 
 export default function LineHeader({
@@ -19,26 +21,32 @@ export default function LineHeader({
   msg,
   msg2,
   price,
+  review,
 }: LineHeaderProps) {
   const isFullWidth = width === '100%';
 
   return (
     <ProdHeader width={width} $padding={padding}>
-      <span>[{msg}]</span>
+      <TitleWrapper>
+        <span>[{msg}]</span>
+        {review && <Review review={review} />}
+      </TitleWrapper>
       <Line $lineWidth={lineWidth} />
       <span>{msg2}</span>
-      <PriceWrapper $isFullWidth={isFullWidth} saleRate={saleRate}>
-        {saleRate && (
-          <SaleInfo>
-            <div />
-            <span>{saleRate} %</span>
-          </SaleInfo>
-        )}
-        <Price $isFullWidth={isFullWidth} saleRate={saleRate}>
-          <label>현재가</label>
-          <label>￦ {price}</label>
-        </Price>
-      </PriceWrapper>
+      {price && (
+        <PriceWrapper $isFullWidth={isFullWidth} saleRate={saleRate}>
+          {saleRate && (
+            <SaleInfo>
+              <div />
+              <span>{saleRate} %</span>
+            </SaleInfo>
+          )}
+          <Price $isFullWidth={isFullWidth} saleRate={saleRate}>
+            <label>현재가</label>
+            <label>￦ {price}</label>
+          </Price>
+        </PriceWrapper>
+      )}
     </ProdHeader>
   );
 }
@@ -51,15 +59,7 @@ const ProdHeader = styled.div<{ width: string; $padding?: string }>`
   width: ${({ width }) => width};
   padding: ${({ $padding }) => $padding};
 
-  span:first-child {
-    color: #1d1d1d;
-    width: 100%;
-    text-align: left;
-    font-size: 10px;
-    font-weight: bold;
-  }
-
-  span:nth-last-child(2) {
+  span:nth-child(3) {
     height: 28.11px;
     color: #0c0c0c;
     width: 100%;
@@ -67,6 +67,18 @@ const ProdHeader = styled.div<{ width: string; $padding?: string }>`
     font-size: 10.81px;
     font-weight: 800;
     line-height: 130%;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  span:first-child {
+    color: #1d1d1d;
+    width: 100%;
+    text-align: left;
+    font-size: 10px;
+    font-weight: bold;
   }
 `;
 
