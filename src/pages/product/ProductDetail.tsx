fba4e32fbaaf97d69e04cf72ed83371afa7a8383
YeChan Kim <az20058@naver.com>
2024-11-22 21:59:03 +0900

    🎨 Design : 이미지 부분 컴포넌트화 및 하트 클릭 시 스타일 변경

--- src/pages/product/ProductDetail.tsx
+++ src/pages/product/ProductDetail.tsx
@@ -1,17 +1,10 @@
 import styled from 'styled-components';
-import Image from '../../components/common/Image';
-import hearticon from '../../assets/images/heartIcon.png';
 import DetailBody from '../../components/productDetail/DetailBody';
-
+import ProductImage from '../../components/productDetail/ProductImage';
 export default function ProductDetail() {
   return (
     <PageWrapper>
-      <ImageWrapper>
-        <Image width="100%" aspectRatio="402/431" src="" borderRadius="20px" />
-        <Heart>
-          <img src={hearticon} />
-        </Heart>
-      </ImageWrapper>
+      <ProductImage />
       <DetailBody />
     </PageWrapper>
   );
@@ -20,29 +13,4 @@ export default function ProductDetail() {
 const PageWrapper = styled.div`
   height: 80vh;
   overflow-y: scroll;
-`;
-
-const ImageWrapper = styled.div`
-  bottom: 20px;
-  width: 100%;
-  position: relative;
-  margin-bottom: -20px;
-`;
-
-const Heart = styled.div`
-  width: 9.45%;
-  aspect-ratio: 1/1;
-  border-radius: 5px;
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
-  background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  img {
-    width: 68.42%;
-    aspect-ratio: 26/23;
-  }
 `;
