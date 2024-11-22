15c8959208a626bb12483f0efc2f6e0b0d09db2c
YeChan Kim <az20058@naver.com>
2024-11-22 22:01:30 +0900

    🎨 Design : 모달 창 배경 요소 흐림 필터 적용

--- src/styles/modal/Modal.styled.ts
+++ src/styles/modal/Modal.styled.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
 
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+`;
+
 export const Wrapper = styled.div`
+  background-color: white;
   border-radius: 21px;
   padding: 3% 3.35%;
   box-sizing: border-box;
