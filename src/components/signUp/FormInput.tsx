9e12b499cd5e0239263591555a59eee6a6a35225
YeChan Kim <az20058@naver.com>
2024-11-29 22:58:28 +0900

    🎨 Design : 회원가입 페이지 컴포넌트 분리 및 UI 완성

---
+++ src/components/signUp/FormInput.tsx
@@ -0,0 +1,145 @@
+import styled from 'styled-components';
+import { UseFormRegister } from 'react-hook-form';
+
+interface FormData {
+  id: string;
+  email: string;
+  password: string;
+  passwordCheck: string;
+  authNumber: string;
+}
+interface FormInputProps {
+  label: string;
+  btnMsg?: string;
+  placeholder: string;
+  register: UseFormRegister<FormData>;
+  name: keyof FormData;
+  type: string;
+  error: string | undefined;
+  onClick?: () => void;
+  errMsg?: string | undefined;
+  checkMsg?: string | undefined;
+  isValidated: boolean;
+}
+
+export default function FormInput({
+  label,
+  btnMsg,
+  placeholder,
+  register,
+  name,
+  type,
+  error,
+  onClick,
+  checkMsg,
+  errMsg,
+  isValidated,
+}: FormInputProps) {
+  return (
+    <InputContainer>
+      <InputLabel>
+        <label>{label}</label>
+        <CheckValid
+          src={
+            type !== 'password'
+              ? checkMsg
+                ? '/images/checked.png'
+                : '/images/notCheck.png'
+              : isValidated
+              ? '/images/checked.png'
+              : '/images/notCheck.png'
+          }
+          alt="Valid"
+        />
+      </InputLabel>
+      <Input $error={!checkMsg ? !!error : !!errMsg}>
+        <input
+          {...register(name)}
+          type={type}
+          placeholder={placeholder}
+          disabled={!!checkMsg && type !== 'password'}
+        />
+        {btnMsg && (
+          <button
+            type="button"
+            onClick={onClick}
+            disabled={!isValidated || !!checkMsg}
+          >
+            {btnMsg}
+          </button>
+        )}
+      </Input>
+      <Message $error={!!error || !!errMsg}>
+        {type !== 'password'
+          ? error || checkMsg || errMsg
+          : isValidated
+          ? checkMsg
+          : error}
+      </Message>
+    </InputContainer>
+  );
+}
+
+const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputLabel = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+const CheckValid = styled.img``;
+
+const Input = styled.div<{ $error?: boolean }>`
+  margin-top: 13px;
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid
+    ${({ theme, $error }) => ($error ? theme.colors.red : 'rgba(0, 0, 0, 0.5)')};
+  padding-bottom: 6px;
+
+  input {
+    padding-left: 3.94%;
+    font-size: 2.74vw;
+    width: 80%;
+    border: none;
+    outline: none;
+
+    &::placeholder {
+      color: #dcdcdc;
+    }
+
+    &:disabled {
+      background-color: #f5f5f5;
+      cursor: not-allowed;
+    }
+  }
+
+  button {
+    font-size: 1.99vw;
+    color: #8f8f8f;
+    background-color: ${({ theme }) => theme.colors.yellow};
+    width: 19.09%;
+    aspect-ratio: 63/23;
+    border-radius: 14px;
+
+    &:disabled {
+      background-color: #f5f5f5;
+      cursor: not-allowed;
+    }
+  }
+`;
+
+const Message = styled.div<{ $error?: boolean }>`
+  margin-top: 10px;
+  font-size: 2.24vw;
+  color: ${({ theme, $error }) =>
+    $error ? theme.colors.red : theme.colors.green};
+  padding-left: 3.94%;
+  text-align: left;
+  height: 13px;
+`;
--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,98 +1,12 @@
 import styled from 'styled-components';
-import InputWrapper from '../../components/signUp/InputWrapper';
-import Button from '../../components/common/Button';
-import { z } from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-const schema = z
-  .object({
-    id: z.string().min(1, { message: '아이디를 입력해주세요' }),
-    email: z.string().email({ message: '이메일 형식이 아닙니다' }),
-    password: z
-      .string()
-      .min(1, { message: '영문,숫자,특수문자 포함 8자 이상이여야 합니다' }),
-    passwordCheck: z
-      .string()
-      .min(1, { message: '비밀번호가 일치하지 않습니다' }),
-    authNumber: z.string().min(1, { message: '인증번호를 입력해주세요' }),
-  })
-  .refine((data) => data.password === data.passwordCheck, {
-    path: ['passwordCheck'],
-    message: '비밀번호가 일치하지 않습니다',
-  });
-type FormData = z.infer<typeof schema>;
+import FormWrapper from '../../components/signUp/FormWrapper';
 
 export default function SignUp() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
-    mode: 'onChange',
-  });
-
-  const onSubmit = (data: FormData) => {
-    console.log(data);
-  };
-
   return (
     <PageWrapper>
       <PageContent>
         <Header>회원가입</Header>
-        <SignUpForm onSubmit={handleSubmit(onSubmit)}>
-          <InputWrapper
-            register={register}
-            name="id"
-            type="text"
-            label="아이디"
-            btnMsg="중복확인"
-            placeholder="아이디를 입력해주세요"
-            error={errors.id?.message}
-          />
-          <InputWrapper
-            register={register}
-            name="email"
-            type="text"
-            label="이메일"
-            placeholder="이메일을 입력해주세요"
-            error={errors.email?.message}
-          />
-          <InputWrapper
-            register={register}
-            name="authNumber"
-            type="text"
-            label="인증번호"
-            placeholder="인증번호 6자리"
-            btnMsg="인증하기"
-            error={errors.authNumber?.message}
-          />
-          <InputWrapper
-            register={register}
-            name="password"
-            type="text"
-            label="비밀번호"
-            placeholder="영문,숫자,특수문자 포함 8자 이상이여야 합니다"
-            error={errors.password?.message}
-          />
-          <InputWrapper
-            register={register}
-            name="passwordCheck"
-            type="text"
-            label="비밀번호 확인"
-            placeholder="영문,숫자,특수문자 포함 8자 이상이여야 합니다"
-            error={errors.passwordCheck?.message}
-          />
-
-          <Button
-            width="100%"
-            onClick={() => {}}
-            borderRadius="100px"
-            aspectRatio="332/36"
-            msg="가입하기"
-          />
-        </SignUpForm>
+        <FormWrapper />
       </PageContent>
     </PageWrapper>
   );
@@ -101,6 +15,7 @@ export default function SignUp() {
 const PageWrapper = styled.div`
   width: 100%;
   box-sizing: border-box;
+  height: 90vh;
   overflow-y: auto;
   padding: 0 8.71%;
   padding-top: 33px;
@@ -125,13 +40,4 @@ const Header = styled.header`
   text-align: left;
   color: #8f8f8f;
   font-weight: 800;
-`;
-
-const SignUpForm = styled.form`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 48px;
-  gap: 36px 0;
 `;
