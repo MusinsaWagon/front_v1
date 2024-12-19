7db142828cbe05230d1bc5899d1814eda140d315
YeChan Kim <az20058@naver.com>
2024-12-19 19:04:11 +0900

    🔧 Settings : tanstack query 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration.ts";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </Provider>
-  </StrictMode>
+    </QueryClientProvider>
+  </Provider>
 );
 serviceWorkerRegistration.register();
