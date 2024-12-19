53ca7a582d889531e8043b6dcf149ef2c07295ae
YeChan Kim <az20058@naver.com>
2024-12-19 19:06:43 +0900

    ✨ Feat : 회원가입 api 연동

--- src/components/signUp/FormWrapper.tsx
+++ src/components/signUp/FormWrapper.tsx
@@ -1,26 +1,13 @@
 import { styled } from 'styled-components';
-import FormInput from './FormInput';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
-import { z } from 'zod';
 import Button from '../common/Button';
-import { schema } from '../../constant/signUpSchema';
-import { getFormInputs } from '../../constant/formInputs';
-
-type FormData = z.infer<typeof schema>;
+import { schema, FormData } from '../../constant/signUpSchema';
+import UserInputContainer from './UserInputContainer';
+import { useMutate } from '../../hooks/useMutation';
+import { signup, UserData } from '../../hooks/signUp/useSignUp';
 
 export default function FormWrapper() {
-  const [checkId, setCheckId] = useState({
-    errMsg: '',
-    checkMsg: '',
-  });
-
-  const [checkEmail, setCheckEmail] = useState({
-    errMsg: '',
-    checkMsg: '',
-  });
-
   const {
     register,
     handleSubmit,
@@ -31,27 +18,31 @@ export default function FormWrapper() {
     mode: 'onChange',
   });
 
-  const onSubmit = (data: FormData) => {
+  const mutation = useMutate(
+    signup,
+    () => {
+      console.log('회원가입 성공!');
+      alert('회원가입이 완료됐습니다.');
+    },
+    () => {
+      console.error('회원가입 실패:');
+    }
+  );
+
+  const onSubmit = (data: UserData) => {
+    mutation.mutate({
+      email: data.email,
+      password: data.password,
+    });
     console.log(data);
   };
 
-  const formInputs = getFormInputs(
-    watch,
-    errors,
-    checkId,
-    setCheckId,
-    checkEmail,
-    setCheckEmail
-  );
-
   return (
     <SignUpForm onSubmit={handleSubmit(onSubmit)}>
-      {formInputs.map((inputProps, index) => (
-        <FormInput key={index} register={register} {...inputProps} />
-      ))}
+      <UserInputContainer watch={watch} register={register} errors={errors} />
       <Button
         width="100%"
-        onClick={() => {}}
+        onClick={() => onSubmit}
         borderRadius="100px"
         aspectRatio="332/36"
         msg="가입하기"
