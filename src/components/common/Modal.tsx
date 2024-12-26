07a5be1aa92c56aa79d46b8127f89105f1a5dbe0
YeChan Kim <az20058@naver.com>
2024-12-26 22:22:13 +0900

    ✨ Feat : Modal 컴포넌트 제작 및 react-modal 설치

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,57 +1,88 @@
-import * as M from '@/styles/modal/Modal.styled';
-import CloseBtn from '@/components/common/CloseBtn.tsx';
-import { getInputInfos } from '../../hooks/modal/getModalInfo';
-import HeaderSection from './HeaderSection';
-import ProductCard from './ProductCard';
-import PriceCard from './PriceCard';
-import UserInput from './UserInput';
+import ReactModal from 'react-modal';
 import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 interface ModalProps {
-  type: string;
-  onClick: () => void;
-  priceList: number[];
-  onClose: () => void;
+  msg: string;
+  setShowModal: (isShow: boolean) => void;
+  showModal: boolean;
 }
 
-export default function Modal({
-  type,
-  onClick,
-  priceList,
-  onClose,
-}: ModalProps) {
-  const inputInfo = getInputInfos(type);
+export default function Modal({ msg, setShowModal, showModal }: ModalProps) {
+  const navigate = useNavigate();
+
+  const customModalStyles: ReactModal.Styles = {
+    overlay: {
+      position: 'absolute',
+      top: '0',
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    },
+    content: {
+      width: '360px',
+      marginTop: '297px',
+      height: '67.62%',
+      position: 'relative',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '10px',
+      boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+      backgroundColor: 'white',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      overflow: 'hidden',
+      animation: 'slideDown 0.3s ease-out', // 애니메이션 추가
+    },
+  };
 
   return (
-    <M.Background>
-      <M.Wrapper>
-        <M.Content>
-          <M.CloseBtnWrapper>
-            <CloseBtn width="5.3%" ratio="1/1" onClick={onClose} />
-          </M.CloseBtnWrapper>
-          <HeaderSection type={type} />
-          <ProductCard label="알림 원하는 상품" />
-          <PriceCard
-            priceList={priceList}
-            aspectRatio="334/64"
-            marginTop="4.59%"
-            marginBottom="5.99%"
-          />
-          <UserInput
-            placeholder={inputInfo.placeholder}
-            label={inputInfo.label}
-            type={inputInfo.type}
-          />
-          <Button
-            width="100%"
-            aspectRatio="358/32"
-            onClick={onClick}
-            borderRadius="100px"
-            marginTop="7.19%"
-            msg="알림 등록하기"
-          />
-        </M.Content>
-      </M.Wrapper>
-    </M.Background>
+    <ModalWrapper>
+      <ReactModal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        style={customModalStyles}
+        ariaHideApp={false}
+        contentLabel="Example Modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <ModalImg src="/images/logo2.png" alt="logo" />
+        <ModalMsg>{msg}</ModalMsg>
+        <Button
+          msg="로그인하기"
+          width="80.6%"
+          borderRadius="100px"
+          onClick={() => navigate('/login')}
+          aspectRatio="324/40"
+        />
+      </ReactModal>
+
+      {/* 애니메이션 정의 */}
+      <style>
+        {`
+          @keyframes slideDown {
+            from {
+              transform: translate(-50%, -100%);
+            }
+            to {
+              transform: translate(-50%, -50%);
+            }
+          }
+        `}
+      </style>
+    </ModalWrapper>
   );
 }
+
+const ModalWrapper = styled.div``;
+const ModalImg = styled.img`
+  margin-top: 72px;
+`;
+const ModalMsg = styled.h2`
+  color: #575757;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 55.59px;
+  margin-bottom: 95px;
+`;
