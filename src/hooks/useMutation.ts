8e257d313a081441945dcecd65215bf46ff91223
YeChan Kim <az20058@naver.com>
2024-12-26 22:29:46 +0900

    ♻️ Refactor : useMutate 훅 파라미터 중 onSuccess, onError 삭제 및 자동화

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -1,13 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 
 export const useMutate = <TData, TVariables>(
-  mutationFn: (variables: TVariables) => Promise<TData>, // 매개변수 타입 적용
-  onSuccess?: (data: TData) => void,
-  onError?: (error: unknown) => void
+  mutationFn: (variables: TVariables) => Promise<TData> // 매개변수 타입 적용
 ) => {
   return useMutation<TData, unknown, TVariables>({
     mutationFn,
-    onSuccess,
-    onError,
+    onSuccess: () => {
+      alert(mutationFn.toString() + '성공');
+    },
+    onError: (error) => {
+      alert(mutationFn.toString() + '실패: ' + error);
+    },
   });
 };
