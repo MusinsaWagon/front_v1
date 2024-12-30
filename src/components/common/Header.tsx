732e9326866bbc61b838615b2c206db2207f5c31
YeChan Kim <az20058@naver.com>
2024-12-30 17:29:41 +0900

    🎨 Design : 전체 컨테이너 높이 40px로 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -32,7 +32,7 @@ const HeaderContainer = styled.div<{ $bottom?: string }>`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 79px;
+  height: 40px;
   justify-content: space-between;
   position: relative;
   bottom: ${(props) => props.$bottom || 'initial'};
