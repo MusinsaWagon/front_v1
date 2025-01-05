7e25e967a494f0acf694ec76366388ceebbb980f
AhnRian <00rian@naver.com>
2025-01-05 18:47:59 +0900

    feat: 카테고리 drawer & axiosInstance public/private 통신 수정

---
+++ src/components/mainPage/categorys/BottomCategory.tsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import { TopCategoryType } from '../types/categories';
+import { useState, useEffect } from 'react';
+import { getBottomCategories } from '../../../apis/drawer/axios';
+import { IoIosArrowForward } from 'react-icons/io';
+import { FaArrowRightToBracket } from 'react-icons/fa6';
+interface BottomCategoryProps {
+  categories: TopCategoryType[];
+  selectedId: number;
+  setIsDrawerVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface SubCategoryType {
+  id: number;
+  categoryName: string;
+}
+
+const BottomCategory: React.FC<BottomCategoryProps> = ({
+  categories,
+  selectedId,
+  setIsDrawerVisible,
+}) => {
+  const [topCategoryName, setTopCategoryName] = useState(
+    categories[selectedId - 1]?.categoryName
+  );
+  const [bottomCategories, setBottomCategories] = useState<SubCategoryType[]>(
+    []
+  );
+  useEffect(() => {
+    if (categories[selectedId - 1]) {
+      setTopCategoryName(categories[selectedId - 1].categoryName);
+    }
+  }, [categories, selectedId]);
+
+  useEffect(() => {
+    const fetchBottomCategories = async () => {
+      if (selectedId > 0) {
+        try {
+          const data = await getBottomCategories(selectedId);
+          setBottomCategories(data.categoryList || []);
+        } catch (error) {
+          console.error('하위 카테고리:', error);
+        }
+      }
+    };
+
+    fetchBottomCategories();
+  }, [selectedId]);
+
+  return (
+    <Container>
+      <TopCategory>
+        {topCategoryName}{' '}
+        <RevertBtn onClick={() => setIsDrawerVisible((visible) => !visible)}>
+          <FaArrowRightToBracket />
+        </RevertBtn>
+      </TopCategory>
+      <BottomCategoryList>
+        {bottomCategories.map((category) => (
+          <Category key={category.id}>
+            {category.categoryName}
+            <Arrow>
+              <IoIosArrowForward />
+            </Arrow>
+          </Category>
+        ))}
+      </BottomCategoryList>
+    </Container>
+  );
+};
+
+export default BottomCategory;
+const Container = styled.div`
+  flex: 1;
+  width: auto;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.black};
+  padding: 0 5px;
+`;
+const TopCategory = styled.div`
+  color: white;
+  height: 8vh;
+  border-bottom: 1px solid #4f4f4f;
+  font-size: 0.7rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px 0 40px;
+`;
+const RevertBtn = styled.button`
+  background-color: transparent;
+  color: white;
+  font-size: 1rem;
+`;
+const BottomCategoryList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+const Category = styled.div`
+  height: 8vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px 0 40px;
+  font-size: 0.7rem;
+  color: white;
+`;
+const Arrow = styled.span`
+  color: white;
+  font-size: 1rem;
+`;
