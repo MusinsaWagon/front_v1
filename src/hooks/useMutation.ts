a93a029a2dbba4f52fc3daed73c4781fdc451e6b
YeChan Kim <az20058@naver.com>
2025-01-07 23:41:08 +0900

    ✨ Feat : 낙관적 업데이트 추가

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -1,13 +1,32 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useMutate = <TData, TVariables>(
-  mutationFn: (variables: TVariables) => Promise<TData>, // 매개변수 타입 적용
+  mutationFn: (variables: TVariables) => Promise<TData>,
   onSuccess?: (data: TData) => void,
-  onError?: (error: unknown) => void
+  onError?: (error: unknown) => void,
+  queryKey?: string // queryKey를 필수로 변경
 ) => {
-  return useMutation<TData, unknown, TVariables>({
+  const queryClient = useQueryClient();
+
+  return useMutation<TData, unknown, TVariables, { previousData?: unknown }>({
     mutationFn,
+    onMutate: async () => {
+      let previousData;
+      if (queryKey) {
+        // 현재 queryKey의 데이터를 저장
+        previousData = queryClient.getQueryData([queryKey]);
+      }
+
+      return { previousData }; // 기본 동작: 이전 데이터 반환
+    },
     onSuccess,
-    onError,
+    onError: (error) => {
+      if (onError) {
+        onError(error);
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey }); // queryKey 관련 캐시 무효화
+    },
   });
 };
